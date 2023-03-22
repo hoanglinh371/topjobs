784e92aaee8060e7cae11fab5e6a847b65c8ec05
Manh Cuong <mrmanhcuong126@gmail.com>
2023-03-22 19:42:26 +0700

    error lol

--- src/components/home/Company.js
+++ src/components/home/Company.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import CompanyLogo from '../shared/CompanyLogo';
-import styles from './Company.module.css';
-import Button from '../shared/Button';
+import CompanyLogo from "../shared/CompanyLogo";
+import styles from "./Company.module.css";
+import Button from "../shared/Button";
 
 const Company = (props) => {
   return (
@@ -17,7 +17,7 @@ const Company = (props) => {
           <h2>{props.company}</h2>
           <p>{props.website}</p>
         </div>
-        <a href={props.website} target='_blank' rel='noreferrer'>
+        <a href={props.website} target="_blank" rel="noreferrer">
           <Button secondary>Company Site</Button>
         </a>
       </div>
