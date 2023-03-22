b8602aad848927fd4fb0d234ad3ee1520f4581d1
hoanglinh371 <hoanglinh371.utc@gmail.com>
2023-03-22 20:47:02 +0700

    updated code

--- src/components/home/JobContent.js
+++ src/components/home/JobContent.js
@@ -1,18 +1,18 @@
-import React from "react";
-import JobInfos from "../shared/JobInfos";
-import Button from "../shared/Button";
-import "./JobDescription.css";
+import React from 'react';
+import JobInfos from '../shared/JobInfos';
+import Button from '../shared/Button';
+import './JobDescription.css';
 
 const JobContent = (props) => {
   return (
-    <div className="jobDescription">
-      <div className="jobDescriptionDetails">
+    <div className='jobDescription'>
+      <div className='jobDescriptionDetails'>
         <div>
           <JobInfos postedAt={props.postedAt} contract={props.contract} />
-          <h1 className="jobDescriptionPosition">{props.position}</h1>
+          <h1 className='jobDescriptionPosition'>{props.position}</h1>
           <h4>{props.location}</h4>
         </div>
-        <a href={props.apply} target="_blank" rel="noreferrer">
+        <a href={props.apply} target='_blank' rel='noreferrer'>
           <Button primary>Apply now</Button>
         </a>
       </div>
@@ -24,34 +24,34 @@ const JobContent = (props) => {
 
       <div>
         <h3>Requirements</h3>
-        {/* <p>{props.requirements.content}</p>  */}
+        {/* <p>{props.requirements['content']}</p> */}
       </div>
       <div>Dat Maid</div>
 
-      {/* <div>
+      <div>
         <h3>Requirements</h3>
         <p>{props.requirements.content}</p>
         <ul>
           {props.requirements.items.map((item, index) => (
             <li key={index}>
-              <span className="jobItemsDots"></span>
+              <span className='jobItemsDots'></span>
               {item}
             </li>
           ))}
         </ul>
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <h3>What You Will Do</h3>
         <p>{props.role.content}</p>
         <ol>
           {props.role.items.map((item, index) => (
             <li key={index}>
-              <span className="jobItemsNumbers">{index + 1}</span>
+              <span className='jobItemsNumbers'>{index + 1}</span>
               {item}
             </li>
           ))}
         </ol>
-      </div> */}
+      </div>
     </div>
   );
 };
